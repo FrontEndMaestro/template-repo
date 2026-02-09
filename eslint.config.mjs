597ead2770d0cb2480640b3c28613da0c9321d69
FrontEndMaestro <abdulahadabbassi2@gmail.com>
2026-02-09 17:03:31 +0500

    Add eslint-plugin-jest in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,18 @@ import config from "eslint-config-standard";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [...[].concat(config), eslintConfigPrettier];
+export default [
+  ...[].concat(config),
+  eslintConfigPrettier,
+  {
+    overrides: [
+      {
+        files: ["tests/**/*"],
+        plugins: ["jest"],
+        env: {
+          "jest/globals": true,
+        },
+      },
+    ],
+  },
+];
